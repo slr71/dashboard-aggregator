5ffc2ab0a96d7572cd6b55979b1699092239fc00
Sarah Roberts <sarahr@cyverse.org>
2020-10-09 15:29:35 -0700

    CORE-1143: add `is_favorite` to app listings

--- src/apps/public.js
+++ src/apps/public.js
@@ -19,11 +19,20 @@ const getQuery = (appIDs) => `
         a.wiki_url,
         a.integration_date,
         a.edited_date,
-        u.username
+        u.username,
+        EXISTS (
+            SELECT * FROM users authenticated_user
+            JOIN workspace w ON authenticated_user.id = w.user_id
+            JOIN app_category_group acg ON w.root_category_id = acg.parent_category_id
+            JOIN app_category_app aca ON acg.child_category_id = aca.app_category_id
+            WHERE authenticated_user.username = $2
+            AND acg.child_index = $3
+            AND aca.app_id = a.id
+         ) AS is_favorite
    FROM apps a
    JOIN integration_data d on a.integration_data_id = d.id
    JOIN users u on d.user_id = u.id
-  WHERE a.id in ( ${appIDs.map((_, index) => `$${index + 2}`).join(",")} )
+  WHERE a.id in ( ${appIDs.map((_, index) => `$${index + 4}`).join(",")} )
     AND a.deleted = false
     AND a.disabled = false
     AND a.integration_date IS NOT NULL
@@ -49,14 +58,16 @@ const getPublicAppIDs = () => {
         });
 };
 
-export const getData = async (db, limit) => {
+export const getData = async (db, username, limit) => {
     const appIDs = await getPublicAppIDs();
 
     const q = getQuery(appIDs);
 
-    const { rows } = await db.query(q, [limit, ...appIDs]).catch((e) => {
-        throw e;
-    });
+    const { rows } = await db
+        .query(q, [limit, username, config.favoritesGroupIndex, ...appIDs])
+        .catch((e) => {
+            throw e;
+        });
 
     if (!rows) {
         throw new Error("no rows returned");
@@ -69,8 +80,9 @@ const getHandler = (db) => async (req, res) => {
     try {
         // the parseInt isn't necessary, but it'll throw an error if the value
         // isn't a number.
+        const username = req.params.username;
         const limit = parseInt(req?.query?.limit ?? "10", 10);
-        const rows = await getData(db, limit);
+        const rows = await getData(db, username, limit);
         res.status(200).json({ apps: rows });
     } catch (e) {
         logger.error(e.message);

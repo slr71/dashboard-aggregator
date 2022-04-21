033d0b29f61266b5f84954fae47d46322822f4d5
Ian McEwen <mian@cyverse.org>
2022-04-20 18:22:31 -0700

    Use abbreviated permissions endpoint for speed

--- src/clients/permissions.js
+++ src/clients/permissions.js
@@ -15,14 +15,14 @@ export const getPublicAppIDs = async () => {
     return tracer().startActiveSpan("getPublicAppIDs", async (span) => {
         try {
             const reqURL = new URL(config.permissionsURL);
-            reqURL.pathname = `/permissions/subjects/group/${config.publicGroup}/app`;
+            reqURL.pathname = `/permissions/abbreviated/subjects/group/${config.publicGroup}/app`;
             const resp = await fetch(reqURL);
             if (!resp.ok) {
                 const msg = await resp.text();
                 throw new Error(msg);
             }
             const data = await resp.json();
-            return data.permissions.map((p) => p.resource.name);
+            return data.permissions.map((p) => p.resource_name);
         } finally {
             span.end();
         }

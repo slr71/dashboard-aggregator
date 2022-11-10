80aca95360249a4b0842f74f56cefa0eb875070c
John Wregglesworth <wregglej@cyverse.org>
2022-11-10 11:26:08 -0700

    Add function that returns public apps

--- db/apps.go
+++ db/apps.go
@@ -58,9 +58,9 @@ func (d *Database) PopularFeaturedApps(ctx context.Context, cfg *PopularFeatured
 			appListingT.Col("integration_date"),
 			appListingT.Col("edited_date"),
 			appListingT.Col("integrator_username").As(goqu.C("username")),
-			goqu.COUNT(jobsT.Col("id")).As(goqu.C("job_count").Eq(goqu.C)),
-			goqu.L("EXISTS(?)", subquery).As("is_favorite"),
-			goqu.L("true").As("is_public"),
+			goqu.COUNT(jobsT.Col("id")).As(goqu.C("job_count")),
+			goqu.L("EXISTS(?)", subquery).As(goqu.C("is_favorite")),
+			goqu.L("true").As(goqu.C("is_public")),
 		).
 		Join(jobsT, goqu.On(jobsT.Col("app_id").Eq(goqu.Cast(appListingT.Col("id"), "TEXT")))).
 		Where(
@@ -101,4 +101,78 @@ func (d *Database) PopularFeaturedApps(ctx context.Context, cfg *PopularFeatured
 	}
 
 	return apps, err
+}
+
+func (d *Database) PublicAppsQuery(ctx context.Context, username string, groupIndex int, publicAppIDs []string, opts ...QueryOption) ([]App, error) {
+	var (
+		err  error
+		db   GoquDatabase
+		apps []App
+	)
+
+	querySettings := &QuerySettings{}
+	for _, opt := range opts {
+		opt(querySettings)
+	}
+
+	if querySettings.tx != nil {
+		db = querySettings.tx
+	} else {
+		db = d.goquDB
+	}
+
+	a := goqu.T("app_listing")
+	w := goqu.T("workspace")
+	acg := goqu.T("app_category_group")
+	aca := goqu.T("app_category_app")
+	u := goqu.T("users")
+
+	subquery := db.From(u).
+		Join(w, goqu.On(u.Col("id").Eq(w.Col("user_id")))).
+		Join(acg, goqu.On(w.Col("root_category_id").Eq(acg.Col("parent_category_id")))).
+		Join(aca, goqu.On(acg.Col("child_category_id").Eq(aca.Col("app_category_id")))).
+		Where(
+			u.Col("username").Eq(username),
+			acg.Col("child_index").Eq(groupIndex),
+			aca.Col("app_id").Eq(a.Col("id")),
+		)
+
+	query := db.From(a).
+		Select(
+			a.Col("id"),
+			goqu.L(`'de'`).As(goqu.C("system_id")),
+			a.Col("name"),
+			a.Col("description"),
+			a.Col("wiki_url"),
+			a.Col("integration_date"),
+			a.Col("edited_date"),
+			a.Col("integrator_username").As(goqu.C("username")),
+			goqu.L("EXISTS(?)", subquery).As(goqu.C("is_favorite")),
+			goqu.L("true").As(goqu.C("is_public")),
+		).
+		Where(
+			a.Col("id").Eq(goqu.Any(publicAppIDs)),
+			a.Col("deleted").Eq(goqu.L("false")),
+			a.Col("disabled").Eq(goqu.L("false")),
+			a.Col("integration_date").IsNotNull(),
+		).
+		Order(
+			a.Col("integration_date").Desc(),
+		)
+
+	if querySettings.hasLimit {
+		query = query.Limit(querySettings.limit)
+	}
+
+	if querySettings.hasOffset {
+		query = query.Offset(querySettings.offset)
+	}
+
+	executor := query.Executor()
+
+	if err = executor.ScanStructsContext(ctx, &apps); err != nil {
+		return nil, err
+	}
+
+	return apps, nil
 }
